af9a2ce7c6f5709feff1d3d3aca7b5cda358cc27
xxMoneyCatxx <guangzhougary@foxmail.com>
2026-06-21 13:42:19 +0800

    perf: #3 PDO持久连接 + #4 关键索引脚本
    #3: db.php PDO_ATTR_PERSISTENT
    
    #4: add_indexes.sql 35表wjid索引
    
    TODO: 同步进度

--- includes/db.php
+++ includes/db.php
@@ -33,7 +33,8 @@ class DB {
                     'collation' => 'utf8mb4_general_ci',
                     'option' => [
                         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                        PDO::ATTR_EMULATE_PREPARES => false
+                        PDO::ATTR_EMULATE_PREPARES => false,
+                        PDO::ATTR_PERSISTENT => true
                     ],
                 ]);
             } catch (Exception $e) {
@@ -44,4 +45,3 @@ class DB {
         return static::$mysql;
     }
 }
-
